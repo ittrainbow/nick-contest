4d29bddd361718b41fb470052467417c11800999
Andrey Gordienko <andreygordieko@b2c.ttk.ru>
2025-10-18 00:00:37 +0300

    fading

--- src/hooks/useFade.ts
+++ src/hooks/useFade.ts
@@ -9,15 +9,16 @@ export const useFade = ({ ref }: { ref: FadeRefType }) => {
   const triggerFade = () => {
     const list = ref.current?.classList
 
-    list?.remove('animate-fade-in-up')
     list?.add('animate-fade-out-down')
 
     setTimeout(() => {
       list?.remove('animate-fade-out-down')
-      list?.remove('animate-fade-out-right')
-      list?.remove('animate-fade-out-left')
       list?.add('animate-fade-in-up')
     }, duration)
+
+    setTimeout(() => {
+      list?.remove('animate-fade-in-up')
+    }, duration * 2)
   }
 
   return { triggerFade }
